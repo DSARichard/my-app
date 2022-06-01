7e1812109d8059ded5c0fd4e94507052b9c0600c
DSARichard <102438643+DSARichard@users.noreply.github.com>
2022-05-31 20:09:06 -0500

    logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
 
 function App() {
-  const str = ("b" + "a" + + "a" + "a").toLowerCase()
-  let num = Math.max()
+  const str = ("b" + "a" + + "a" + "a").toLowerCase();
+  let num = Math.max();
   return (
     <div className="container">
       <Header />
-      <img src={logo} className="App-logo" alt="logo" />
       <div style={{ color: "goldenrod", }}>{str}</div>
       <div style={{ color: "goldenrod", }}>{num}</div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import logo from '../logo.svg';
 
 const Header = ({ title }) => {
   return (
     <header>
+      <img src={logo} className="App-logo" alt="logo" />
       <h1>{title}</h1>
     </header>
-  )
-}
+  );
+};
 Header.propTypes = {
   title: PropTypes.string,
-}
+};
 Header.defaultProps = {
   title: "Task Tracker",
-}
+};
 
-export default Header
+export default Header;
